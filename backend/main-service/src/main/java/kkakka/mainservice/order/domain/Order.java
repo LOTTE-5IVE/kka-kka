4469097d9cb2f9035b9036419aa1ac3f4a985977
devpooh <huhu708@naver.com>
2022-09-26 15:47:16 +0900

    refactor: 엔티티 pk 이름지정 삭제

--- backend/main-service/src/main/java/kkakka/mainservice/order/domain/Order.java
+++ backend/main-service/src/main/java/kkakka/mainservice/order/domain/Order.java
@@ -12,12 +12,11 @@ import java.util.List;
 public class Order {
 
     @Id @GeneratedValue
-    @Column(name = "order_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
-    private Member member;
+    private Member member; //주문회원
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<ProductOrder> productOrders = new ArrayList<>();
