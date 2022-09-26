6a8f1833665873278f014cdfd020dbbe75bc384d
Solji Choi <bssj9307@gmail.com>
2022-09-26 10:39:50 +0900

    feat: 필드명 수정

--- backend/main-service/src/main/java/kkakka/mainservice/auth/infrastructure/naver/dto/NaverTokenRequest.java
+++ backend/main-service/src/main/java/kkakka/mainservice/auth/infrastructure/naver/dto/NaverTokenRequest.java
@@ -1,5 +1,6 @@
 package kkakka.mainservice.auth.infrastructure.naver.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,12 @@ import lombok.NoArgsConstructor;
 @Getter
 public class NaverTokenRequest {
 
-    private String grant_type;
-    private String client_id;
-    private String client_secret;
+    @JsonProperty(value = "grant_type")
+    private String grantType;
+    @JsonProperty(value = "client_id")
+    private String clientId;
+    @JsonProperty(value = "client_secret")
+    private String clientSecret;
     private String code;
     private String state;
 }
