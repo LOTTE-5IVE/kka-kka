bb5338ce1d71baf40fb86976cbba71f91bfc4440
Solji Choi <bssj9307@gmail.com>
2022-09-27 18:21:20 +0900

    feat: RestDocs를 사용한 API 문서화 구현 - 테스트 코드에 반영

--- backend/main-service/src/test/java/kkakka/mainservice/member/acceptance/LoginAcceptanceTest.java
+++ backend/main-service/src/test/java/kkakka/mainservice/member/acceptance/LoginAcceptanceTest.java
@@ -2,11 +2,12 @@ package kkakka.mainservice.member.acceptance;
 
 import static kkakka.mainservice.fixture.TestMember.MEMBER_01;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import kkakka.mainservice.AcceptanceTest;
+import kkakka.mainservice.DocumentConfiguration;
 import kkakka.mainservice.auth.domain.ProviderName;
 import kkakka.mainservice.auth.ui.dto.SocialProviderCodeRequest;
 import org.junit.jupiter.api.DisplayName;
@@ -14,7 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-public class LoginAcceptanceTest extends AcceptanceTest {
+public class LoginAcceptanceTest extends DocumentConfiguration {
 
     @DisplayName("회원가입과 토큰 발급 - 성공")
     @Test
@@ -24,11 +25,13 @@ public class LoginAcceptanceTest extends AcceptanceTest {
                 MEMBER_01.getCode(), ProviderName.TEST);
 
         // when
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+        final ExtractableResponse<Response> response = RestAssured
+                .given(spec).log().all()
+                .filter(document("join-success"))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(request)
                 .when()
-                .post("/auth/login/token")
+                .post("/api/auth/login/token")
                 .then().log().all().extract();
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
@@ -37,18 +40,20 @@ public class LoginAcceptanceTest extends AcceptanceTest {
 
     @DisplayName("로그인과 토큰 발급 - 성공")
     @Test
-    void loginSuccess(){
+    void loginSuccess() {
         // given
         final SocialProviderCodeRequest request = SocialProviderCodeRequest.create(
                 MEMBER_01.getCode(), ProviderName.TEST);
         회원가입_요청(request);
 
         // when
-        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+        final ExtractableResponse<Response> response = RestAssured
+                .given(spec).log().all()
+                .filter(document("login-success"))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(request)
                 .when()
-                .post("/auth/login/token")
+                .post("/api/auth/login/token")
                 .then().log().all().extract();
 
         // then
