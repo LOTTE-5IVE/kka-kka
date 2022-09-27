9dff428aaee9061654eec67d192df0edcc6141c5
Solji Choi <bssj9307@gmail.com>
2022-09-27 10:40:19 +0900

    refactor: value source 수정

--- backend/main-service/src/main/java/kkakka/mainservice/member/auth/util/JwtTokenProvider.java
+++ backend/main-service/src/main/java/kkakka/mainservice/member/auth/util/JwtTokenProvider.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
-@PropertySource("classpath:bootstrap.yml")
 public class JwtTokenProvider {
 
     @Value("${security.jwt.token.secret-key}")
