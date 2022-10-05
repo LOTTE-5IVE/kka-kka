56f7e2c342939f8d8b4ce3926ec491c71e1dd6ff
devpooh <huhu708@naver.com>
2022-10-05 10:53:19 +0900

    refactor: 코드리뷰 반영

--- backend/main-service/src/test/java/kkakka/mainservice/order/application/OrderServiceTest.java
+++ backend/main-service/src/test/java/kkakka/mainservice/order/application/OrderServiceTest.java
@@ -4,10 +4,11 @@ import kkakka.mainservice.member.domain.Member;
 import kkakka.mainservice.member.domain.repository.MemberRepository;
 import kkakka.mainservice.order.domain.Order;
 import kkakka.mainservice.order.domain.repository.OrderRepository;
-import kkakka.mainservice.order.ui.dto.OrderRequestDto;
+import kkakka.mainservice.order.ui.dto.OrderRequest;
 import kkakka.mainservice.order.ui.dto.ProductOrderRequest;
 import kkakka.mainservice.product.domain.Product;
 import kkakka.mainservice.product.domain.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -49,61 +50,63 @@ class OrderServiceTest {
     public void productOrderOneTest_success() throws Exception {
 
         //given
-        ProductOrderRequest productOrderRequest1 = new ProductOrderRequest(1L,2);
+        ProductOrderRequest productOrderRequest1 = new ProductOrderRequest(1L, 2);
         List<ProductOrderRequest> productOrderRequests = new ArrayList<>();
         productOrderRequests.add(productOrderRequest1);
 
-        OrderRequestDto orderRequestDto = new OrderRequestDto(member.getId(), productOrderRequests);
+        OrderRequest orderRequest = new OrderRequest(member.getId(), productOrderRequests);
 
         //when
-        Long orderId = orderService.order(orderRequestDto);
+        Long orderId = orderService.order(orderRequest);
 
         //then
         Order getOrder = orderRepository.findById(orderId).orElseThrow();
 
         assertThat(orderId).isEqualTo(getOrder.getId());
-        assertThat(2000*2).isEqualTo(getOrder.getTotalPrice());
+        assertThat(2000 * 2).isEqualTo(getOrder.getTotalPrice());
     }
 
     @Test
     @DisplayName("상품 2개 이상 주문 - 성공")
-    public void productOrderMoreTest_success() throws Exception {
+    public void productOrderMoreTest_success() {
 
         //given
-        ProductOrderRequest productOrderRequest1 = new ProductOrderRequest(1L,2);
-        ProductOrderRequest productOrderRequest2 = new ProductOrderRequest(2L,3);
+        ProductOrderRequest productOrderRequest1 = new ProductOrderRequest(1L, 2);
+        ProductOrderRequest productOrderRequest2 = new ProductOrderRequest(2L, 3);
 
         List<ProductOrderRequest> productOrderRequests = new ArrayList<>();
         productOrderRequests.add(productOrderRequest1);
         productOrderRequests.add(productOrderRequest2);
 
-        OrderRequestDto orderRequestDto = new OrderRequestDto(member.getId(), productOrderRequests);
+        OrderRequest orderRequest = new OrderRequest(member.getId(), productOrderRequests);
 
         //when
-        Long orderId = orderService.order(orderRequestDto);
+        Long orderId = orderService.order(orderRequest);
 
         //then
         Order getOrder = orderRepository.findById(orderId).orElseThrow();
 
         assertThat(orderId).isEqualTo(getOrder.getId());
-        assertThat(2000*2+1000*3).isEqualTo(getOrder.getTotalPrice());
+        assertThat(2000 * 2 + 1000 * 3).isEqualTo(getOrder.getTotalPrice());
     }
 
     @Test
     @DisplayName("상품주문 - 실패(재고수량초과)")
-    public void productOrder_fail_inventoryExceeded() throws Exception {
+    public void productOrder_fail_inventoryExceeded() {
 
         //given
-        ProductOrderRequest productOrderRequest1 = new ProductOrderRequest(1L,11);
+        ProductOrderRequest productOrderRequest1 = new ProductOrderRequest(1L, 11);
         List<ProductOrderRequest> productOrderRequests = new ArrayList<>();
         productOrderRequests.add(productOrderRequest1);
 
-        OrderRequestDto orderRequestDto = new OrderRequestDto(member.getId(), productOrderRequests);
+        OrderRequest orderRequest = new OrderRequest(member.getId(), productOrderRequests);
 
         //when
-        orderService.order(orderRequestDto);
-
         //then
-        fail("재고 수량 부족으로 예외가 발생해야 한다.");
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+           orderService.order(orderRequest);
+        });
     }
+
+
 }
