f2550aa064403bd4f732dfe5c8443f0f268dd704
Solji Choi <bssj9307@gmail.com>
2022-10-05 11:14:23 +0900

    feat: 회원 정보 수정 인수테스트 및 문서화 작성

--- backend/main-service/src/test/java/kkakka/mainservice/member/acceptance/MemberAcceptanceTest.java
+++ backend/main-service/src/test/java/kkakka/mainservice/member/acceptance/MemberAcceptanceTest.java
@@ -2,6 +2,7 @@ package kkakka.mainservice.member.acceptance;
 
 import static kkakka.mainservice.fixture.TestMember.MEMBER_01;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
@@ -9,6 +10,7 @@ import io.restassured.response.Response;
 import kkakka.mainservice.DocumentConfiguration;
 import kkakka.mainservice.member.auth.ui.dto.SocialProviderCodeRequest;
 import kkakka.mainservice.member.member.domain.MemberProviderName;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -48,6 +50,79 @@ public class MemberAcceptanceTest extends DocumentConfiguration {
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @DisplayName("회원 정보 수정 요청(이름 수정) - 성공")
+    @Test
+    void editMemberInfo_name_success() {
+        // given
+        final String accessToken = 액세스_토큰_가져옴();
+        final String newName = "새로운이름";
+
+        // when
+        final ExtractableResponse<Response> response = RestAssured
+                .given(spec).log().all()
+                .filter(document("member-info-update-name"))
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(String.format("{\"name\": \"%s\"}", newName))
+                .when()
+                .patch("/api/members/me")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @DisplayName("회원 정보 수정 요청(이메일, 연락처) - 성공")
+    @Test
+    void editMemberInfo_email_phone_success() {
+        // given
+        final String accessToken = 액세스_토큰_가져옴();
+        final String newMail = "validname@test.com";
+        final String newPhone = "010-0000-0001";
+
+        // when
+        final ExtractableResponse<Response> response = RestAssured
+                .given(spec).log().all()
+                .filter(document("member-info-update-email-phone"))
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(String.format("{\"email\": \"%s\", \"phone\": \"%s\"}", newMail, newPhone))
+                .when()
+                .patch("/api/members/me")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    // TODO: 회원 정보 조회 API 구현한 뒤 이어서 작성
+    @Disabled
+    @DisplayName("회원 정보 수정 요청(이메일, 연락처) - 실패")
+    @Test
+    void editMemberInfo_email_phone_fail() {
+        // given
+        final String accessToken = 액세스_토큰_가져옴();
+        final String newMail = "invalid-email";
+        final String newPhone = "010-0000-0001";
+
+        // when
+        final ExtractableResponse<Response> response = RestAssured
+                .given(spec).log().all()
+                .filter(document("member-info-update-email-phone"))
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(String.format("{\"email\": \"%s\", \"phone\": \"%s\"}", newMail, newPhone))
+                .when()
+                .patch("/api/members/me")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
     private String 액세스_토큰_가져옴() {
