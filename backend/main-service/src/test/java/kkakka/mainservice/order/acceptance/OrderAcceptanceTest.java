15d12ceefca14b57b2e567a1d9cd95966e468172
Solji Choi <bssj9307@gmail.com>
2022-10-13 09:56:39 +0900

    refactor: 주문내역 테스트 기존 픽스쳐에 맞게 리팩토링

--- backend/main-service/src/test/java/kkakka/mainservice/order/acceptance/OrderAcceptanceTest.java
+++ backend/main-service/src/test/java/kkakka/mainservice/order/acceptance/OrderAcceptanceTest.java
@@ -1,30 +1,53 @@
 package kkakka.mainservice.order.acceptance;
 
-import static kkakka.mainservice.fixture.TestMember.*;
-import static org.assertj.core.api.Assertions.*;
+import static kkakka.mainservice.cart.TestDataLoader.PRODUCT_1;
+import static kkakka.mainservice.cart.TestDataLoader.PRODUCT_2;
+import static kkakka.mainservice.fixture.TestMember.MEMBER_01;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import kkakka.mainservice.DocumentConfiguration;
 import kkakka.mainservice.member.auth.ui.dto.SocialProviderCodeRequest;
 import kkakka.mainservice.member.member.domain.MemberProviderName;
 import kkakka.mainservice.order.application.dto.ProductOrderDto;
 import kkakka.mainservice.order.ui.dto.OrderRequest;
-import kkakka.mainservice.product.domain.repository.ProductRepository;
+import org.hibernate.Session;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 public class OrderAcceptanceTest extends DocumentConfiguration {
 
-    @Autowired
-    ProductRepository productRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @AfterEach
+    public void tearDown() {
+        entityManager.unwrap(Session.class)
+                .doWork(this::cleanUpTable);
+    }
+
+    private void cleanUpTable(Connection conn) throws SQLException {
+        Statement statement = conn.createStatement();
+        statement.executeUpdate("SET REFERENTIAL_INTEGRITY FALSE");
+
+        statement.executeUpdate("TRUNCATE TABLE \"order\"");
+        statement.executeUpdate("TRUNCATE TABLE product_order");
+
+        statement.executeUpdate("SET REFERENTIAL_INTEGRITY TRUE");
+    }
 
     @DisplayName("상품 주문 - 성공")
     @Test
@@ -32,8 +55,8 @@ public class OrderAcceptanceTest extends DocumentConfiguration {
         //given
         String accessToken = 액세스_토큰_가져옴();
 
-        ProductOrderDto productOrderDto1 = new ProductOrderDto(1L,2);
-        ProductOrderDto productOrderDto2 = new ProductOrderDto(2L,1);
+        ProductOrderDto productOrderDto1 = new ProductOrderDto(PRODUCT_1.getId(), 2);
+        ProductOrderDto productOrderDto2 = new ProductOrderDto(PRODUCT_2.getId(), 1);
         List<ProductOrderDto> productOrderDtos = new ArrayList<>();
         productOrderDtos.add(productOrderDto1);
         productOrderDtos.add(productOrderDto2);
@@ -41,15 +64,15 @@ public class OrderAcceptanceTest extends DocumentConfiguration {
 
         //when
         final ExtractableResponse<Response> response = RestAssured
-            .given(spec).log().all()
-            .filter(document("product-order-success"))
-            .header("Authorization","Bearer " + accessToken)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(orderRequest)
-            .when()
-            .post("api/orders")
-            .then().log().all()
-            .extract();
+                .given(spec).log().all()
+                .filter(document("product-order-success"))
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(orderRequest)
+                .when()
+                .post("api/orders")
+                .then().log().all()
+                .extract();
 
         //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
@@ -60,33 +83,53 @@ public class OrderAcceptanceTest extends DocumentConfiguration {
     void findMemberOrders_success() {
         //given
         String accessToken = 액세스_토큰_가져옴();
+        주문_요청함(accessToken, PRODUCT_1.getId());
+        주문_요청함(accessToken, PRODUCT_2.getId());
 
         //when
         final ExtractableResponse<Response> response = RestAssured
-            .given(spec).log().all()
-            .filter(document("orders-info-member-success"))
-            .header("Authorization","Bearer " + accessToken)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .get("api/orders/me")
-            .then().log().all()
-            .extract();
+                .given(spec).log().all()
+                .filter(document("orders-info-member-success"))
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("api/orders/me")
+                .then().log().all()
+                .extract();
 
         //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.body().as(List.class)).hasSize(2);
+    }
+
+    private void 주문_요청함(String accessToken, Long productId) {
+        OrderRequest orderRequest = new OrderRequest(
+                List.of(new ProductOrderDto(productId, 1))
+        );
+
+        //when
+        RestAssured
+                .given().log().all()
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(orderRequest)
+                .when()
+                .post("/api/orders")
+                .then().log().all()
+                .extract();
     }
 
     private String 액세스_토큰_가져옴() {
         final SocialProviderCodeRequest request = SocialProviderCodeRequest.create(
-            MEMBER_01.getCode(), MemberProviderName.TEST);
+                MEMBER_01.getCode(), MemberProviderName.TEST);
 
         final ExtractableResponse<Response> response = RestAssured
-            .given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(request)
-            .when()
-            .post("/api/login/token")
-            .then().log().all().extract();
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/api/login/token")
+                .then().log().all().extract();
 
         return response.body().jsonPath().get("accessToken");
     }
