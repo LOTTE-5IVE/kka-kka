db232c31339a4e396a16df1a798651e056792b40
Solji Choi <bssj9307@gmail.com>
2022-10-18 12:36:23 +0900

    feat: 불필요한 import 문 제거

--- backend/main-service/src/main/java/kkakka/mainservice/common/config/WebMvcConfig.java
+++ backend/main-service/src/main/java/kkakka/mainservice/common/config/WebMvcConfig.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
