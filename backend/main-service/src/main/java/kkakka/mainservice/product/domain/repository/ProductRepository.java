9dc133a46ac8a08f5d97c1de035c983b31e7d8a2
Solji Choi <bssj9307@gmail.com>
2022-10-18 16:03:52 +0900

    fix: CategoryRepository, ProductRepository 메서드 변경 반영

--- backend/main-service/src/main/java/kkakka/mainservice/product/domain/repository/ProductRepository.java
+++ backend/main-service/src/main/java/kkakka/mainservice/product/domain/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package kkakka.mainservice.product.domain.repository;
 
+import java.util.List;
 import kkakka.mainservice.product.domain.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,4 +17,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query(value = "select p from Product p where p.category.id = :categoryId",
             countQuery = "select count(p) from Product p where p.category.id = :categoryId")
     Page<Product> findByCategoryId(@Param("categoryId") Long categoryId, Pageable pageable);
+
+    @Query(value = "select p from Product p where p.category.id = :categoryId")
+    List<Product> findByCategoryId(@Param("categoryId") Long categoryId);
 }
