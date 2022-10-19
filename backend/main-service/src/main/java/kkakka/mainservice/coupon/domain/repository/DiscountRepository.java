fe1031dcec43e04e4bb87b93930ec7fcdea23d45
omj9803 <myeongju0316@gmail.com>
2022-10-19 17:40:55 +0900

    fix: API 연결, 삭제된 할인은 목록에서 삭제하도록 수정

--- backend/main-service/src/main/java/kkakka/mainservice/coupon/domain/repository/DiscountRepository.java
+++ backend/main-service/src/main/java/kkakka/mainservice/coupon/domain/repository/DiscountRepository.java
@@ -1,8 +1,12 @@
 package kkakka.mainservice.coupon.domain.repository;
 
+import java.util.List;
 import kkakka.mainservice.coupon.domain.Discount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DiscountRepository extends JpaRepository<Discount, Long> {
 
+    @Query("select d from Discount d where d.isDeleted = false")
+    List<Discount> findAllDiscountsNotDeleted();
 }
