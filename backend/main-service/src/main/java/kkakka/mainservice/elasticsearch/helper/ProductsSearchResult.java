cb55368d4ac1148ceb6ffddd544891514102079b
devpooh <huhu708@naver.com>
2022-11-05 21:55:19 +0900

    refator: es로직 인터페이스화

---
+++ backend/main-service/src/main/java/kkakka/mainservice/elasticsearch/helper/ProductsSearchResult.java
@@ -0,0 +1,18 @@
+package kkakka.mainservice.elasticsearch.helper;
+
+import java.util.List;
+import kkakka.mainservice.common.dto.PageInfo;
+import kkakka.mainservice.product.domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.elasticsearch.core.SearchPage;
+
+@AllArgsConstructor
+@Getter
+public class ProductsSearchResult {
+
+    private List<Product> products;
+    private PageInfo pageInfo;
+    private Long totalHits;
+
+}
