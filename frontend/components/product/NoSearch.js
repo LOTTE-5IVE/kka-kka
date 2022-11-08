70250d58c4c3771b1fcf69bfe80876135bf333d8
devpooh <huhu708@naver.com>
2022-11-08 12:02:00 +0900

    feat: 검색 기능 구현

---
+++ frontend/components/product/NoSearch.js
@@ -0,0 +1,100 @@
+export default function NoSearch() {
+  return (
+    <>
+      <div className="NotFoundWrapper">
+        <div className="image">
+          <img src="/404.png" />
+        </div>
+        <div className="comment">
+          <span>까까에는 이런 상품이 없어요 ㅠㅠ</span>
+          <span>다른 키워드로 검색해주세요!</span>
+        </div>
+
+        <style jsx>{`
+          @media screen and (min-width: 769px) {
+            /* 데스크탑에서 사용될 스타일을 여기에 작성합니다. */
+            .NotFoundWrapper {
+              position: relative;
+              top: 10%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+
+              .image {
+                display: flex;
+                justify-content: center;
+
+                img {
+                  width: 200px;
+                }
+              }
+
+              .comment {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                text-align: center;
+                font-size: 30px;
+              }
+            }
+          }
+
+          @media screen and (max-width: 768px) {
+            /* 태블릿에 사용될 스트일 시트를 여기에 작성합니다. */
+            .NotFoundWrapper {
+              width: 100vw;
+              position: relative;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+
+              .image {
+                display: flex;
+                justify-content: center;
+
+                img {
+                  width: 200px;
+                }
+              }
+
+              .comment {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                text-align: center;
+                font-size: 20px;
+              }
+            }
+          }
+
+          @media screen and (max-width: 480px) {
+            /* 모바일에 사용될 스트일 시트를 여기에 작성합니다. */
+            .NotFoundWrapper {
+              width: 100vw;
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+
+              .image {
+                display: flex;
+                justify-content: center;
+
+                img {
+                  width: 150px;
+                }
+              }
+
+              .comment {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                text-align: center;
+                font-size: 15px;
+              }
+            }
+          }
+        `}</style>
+      </div>
+    </>
+  );
+}
