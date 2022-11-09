8534168fe48d5c5ef116662c42d6476641a56526
tlsdnwn55 <tlsdnwn55@naver.com>
2022-11-09 10:18:23 +0900

    feature: 장바구니 쿠폰 우효기간 체크 추가

--- backend/main-service/src/main/java/kkakka/mainservice/cart/application/CartService.java
+++ backend/main-service/src/main/java/kkakka/mainservice/cart/application/CartService.java
@@ -7,7 +7,6 @@ import kkakka.mainservice.cart.domain.repository.CartRepository;
 import kkakka.mainservice.cart.ui.dto.CartItemDto;
 import kkakka.mainservice.cart.ui.dto.CartRequestDto;
 import kkakka.mainservice.cart.ui.dto.CartResponseDto;
-import kkakka.mainservice.cart.ui.dto.CouponDto;
 import kkakka.mainservice.common.exception.KkaKkaException;
 import kkakka.mainservice.common.exception.NotFoundMemberException;
 import kkakka.mainservice.common.exception.NotOrderOwnerException;
@@ -22,8 +21,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -58,11 +59,28 @@ public class CartService {
                 .orElseThrow(KkaKkaException::new);
 
         Cart cart = findOrCreateCart(member);
-        final List<CartItemDto> cartItemDtos = cart.getCartItems().stream()
-                .map(CartItemDto::from)
+        List<CartItem> cartItems = cart.getCartItems();
+
+        Stream<CartItemDto> couponIsPresent = cartItems.stream()
+                .filter(cartItem -> isCouponApplied(cartItem))
+                .map(cartItem -> CartItemDto.createWithCoupon(cartItem,
+                        cartItem.getDiscountedPrice(cartItem.getCoupon()),
+                        cartItem.getCoupon())
+                );
+        Stream<CartItemDto> couponIsNotPresent = cartItems.stream()
+                .filter(cartItem -> !isCouponApplied(cartItem))
+                .map(CartItemDto::createWithoutCoupon);
+
+        final List<CartItemDto> cartItemDtos = Stream.concat(couponIsPresent, couponIsNotPresent)
+                .sorted(Comparator.comparing(CartItemDto::getCartItemId))
                 .collect(Collectors.toList());
 
         return new CartResponseDto(cart.getId(), cartItemDtos);
+    }
+
+    private boolean isCouponApplied(CartItem cartItem) {
+        Coupon coupon = cartItem.getCoupon();
+        return coupon != null && coupon.isNotExpired();
     }
 
     private CartItem findOrCreateCartItem(Product product, Cart cart) {
@@ -91,10 +109,11 @@ public class CartService {
 
         CartItem cartItem = cartItemRepository.findByIdandMemberId(cartItemId, loginMemberId).orElseThrow(KkaKkaException::new);
         Coupon coupon = couponRepository.findById(couponId).orElseThrow(KkaKkaException::new);
+        cartItem.getProduct().getDiscount()
         cartItem.applyCoupon(coupon);
         Integer discountedPrice = cartItem.getDiscountedPrice(coupon);
 
-        return CartItemDto.applyCouponDto(cartItem, discountedPrice, CouponDto.toDto(coupon));
+        return CartItemDto.createWithCoupon(cartItem, discountedPrice, coupon);
     }
 
     private Cart findOrCreateCart(Member member) {
