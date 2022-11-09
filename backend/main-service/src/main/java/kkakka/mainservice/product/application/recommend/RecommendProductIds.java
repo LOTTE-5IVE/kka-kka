a6067d7635b749820cf6a46d44d9ef50cac53e39
Solji Choi <bssj9307@gmail.com>
2022-11-09 21:00:51 +0900

    feat: redis 의존성 추가 및 설정, 기본 테스트

--- backend/main-service/src/main/java/kkakka/mainservice/product/application/recommend/RecommendProductIds.java
+++ backend/main-service/src/main/java/kkakka/mainservice/product/application/recommend/RecommendProductIds.java
@@ -6,10 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@RedisHash("recommend")
 public class RecommendProductIds {
 
     @Id
---
+++ backend/main-service/src/main/java/kkakka/mainservice/product/infrastructure/redis/RedisConfig.java
@@ -0,0 +1,32 @@
+package kkakka.mainservice.product.infrastructure.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
