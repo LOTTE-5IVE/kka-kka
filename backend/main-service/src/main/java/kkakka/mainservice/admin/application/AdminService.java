70e6187b7e74fe1742ea02b8f4b214fef4be9cbf
Solji Choi <bssj9307@gmail.com>
2022-11-14 18:12:51 +0900

    feat: admin 로그인 구현

--- backend/main-service/src/main/java/kkakka/mainservice/admin/application/AdminService.java
+++ backend/main-service/src/main/java/kkakka/mainservice/admin/application/AdminService.java
@@ -1,7 +1,29 @@
 package kkakka.mainservice.admin.application;
 
+import kkakka.mainservice.admin.ui.dto.AdminUserRequest;
+import kkakka.mainservice.member.auth.exception.AuthorizationException;
+import kkakka.mainservice.member.auth.util.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AdminService {
+
+    @Value("${admin.user.id}")
+    private String adminId;
+    @Value("${admin.user.password}")
+    private String adminPassword;
+    @Value("${admin.user.key}")
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public String requiredLogin(AdminUserRequest adminUserRequest) {
+        if (adminUserRequest.getUserId().equals(adminId)
+                && adminUserRequest.getPassword().equals(adminPassword)) {
+            return jwtTokenProvider.generateToken(adminId);
+        }
+        throw new AuthorizationException();
+    }
 }
