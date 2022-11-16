084ef7321c0567e905ebec07a539295ad32ce1e1
hyeyeon kim <99088509+devpoooh@users.noreply.github.com>
2022-11-16 15:39:20 +0900

    fix: url 변수명 수정 (#271)

--- backend/main-service/src/main/java/kkakka/mainservice/common/config/ElasticSearchConfig.java
+++ backend/main-service/src/main/java/kkakka/mainservice/common/config/ElasticSearchConfig.java
@@ -13,7 +13,7 @@ import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfig
 @Configuration
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
-    @Value("${elasticsearch.url2}")
+    @Value("${elasticsearch.url}")
     private String elasticsearchUrl;
     @Value("${elasticsearch.user_name}")
     private String userName;
