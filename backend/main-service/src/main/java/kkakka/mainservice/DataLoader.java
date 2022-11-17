001155f4ea6dba791eecf8f5f438a4f7eab59137
soulG <bssj9307@gmail.com>
2022-11-17 15:30:55 +0900

    refactor: 테스트 데이터 삭제, 카테고리 오류 수정 (#293)

--- backend/main-service/src/main/java/kkakka/mainservice/DataLoader.java
+++ backend/main-service/src/main/java/kkakka/mainservice/DataLoader.java
@@ -1,28 +1,14 @@
 package kkakka.mainservice;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import kkakka.mainservice.category.domain.Category;
 import kkakka.mainservice.category.domain.repository.CategoryRepository;
-import kkakka.mainservice.member.member.domain.Member;
-import kkakka.mainservice.member.member.domain.Provider;
-import kkakka.mainservice.member.member.domain.ProviderName;
-import kkakka.mainservice.member.member.domain.repository.MemberRepository;
-import kkakka.mainservice.order.application.dto.RecipientDto;
-import kkakka.mainservice.order.domain.Order;
-import kkakka.mainservice.order.domain.ProductOrder;
-import kkakka.mainservice.order.domain.Recipient;
-import kkakka.mainservice.order.domain.repository.OrderRepository;
-import kkakka.mainservice.order.domain.repository.ProductOrderRepository;
 import kkakka.mainservice.product.domain.Nutrition;
 import kkakka.mainservice.product.domain.Product;
 import kkakka.mainservice.product.domain.repository.NutritionRepository;
 import kkakka.mainservice.product.domain.repository.ProductRepository;
-import kkakka.mainservice.review.domain.Review;
-import kkakka.mainservice.review.domain.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,16 +19,10 @@ public class DataLoader {
 
     private final CategoryRepository categoryRepository;
     private final ProductRepository productRepository;
-    private final MemberRepository memberRepository;
-    private final ProductOrderRepository productOrderRepository;
-    private final OrderRepository orderRepository;
-    private final ReviewRepository reviewRepository;
     private final NutritionRepository nutritionRepository;
 
     public static final Map<String, Category> categories = new LinkedHashMap<>();
     private boolean categoryFlag = false;
-    private Product testProduct;
-    private Member testMember;
 
     @Transactional
     public void saveData(List<String[]> data) {
@@ -51,89 +31,9 @@ public class DataLoader {
             return;
         }
 
-        List<Product> products = new ArrayList<>();
         for (String[] datum : data) {
-            products.add(saveProduct(datum));
+            saveProduct(datum);
         }
-        testProduct = products.get(0);
-
-        saveUser();
-        saveOrderAndReview();
-    }
-
-    private void saveOrderAndReview() {
-        final List<ProductOrder> productOrders = productOrderRepository.saveAll(
-                Arrays.asList(
-                        ProductOrder.create(testProduct, testProduct.getPrice(), testProduct.getDiscount(), 1),
-                        ProductOrder.create(testProduct, testProduct.getPrice(), testProduct.getDiscount(), 1),
-                        ProductOrder.create(testProduct, testProduct.getPrice(), testProduct.getDiscount(), 1),
-                        ProductOrder.create(testProduct, testProduct.getPrice(), testProduct.getDiscount(), 1),
-                        ProductOrder.create(testProduct, testProduct.getPrice(), testProduct.getDiscount(), 1),
-                        ProductOrder.create(testProduct, testProduct.getPrice(), testProduct.getDiscount(), 1),
-                        ProductOrder.create(testProduct, testProduct.getPrice(), testProduct.getDiscount(), 1)
-                ));
-
-        Recipient testRecipient = Recipient.from(RecipientDto.create(
-                testMember.getName(), testMember.getEmail(), testMember.getPhone(),
-                testMember.getAddress()
-        ));
-        orderRepository.save(
-                Order.create(testMember, testRecipient, testProduct.getPrice(), productOrders));
-        reviewRepository.save(Review.create("test-review1", 5.0, testMember, productOrders.get(0)));
-        reviewRepository.save(Review.create("test-review2", 3.5, testMember, productOrders.get(1)));
-        reviewRepository.save(Review.create("test-review3", 4.5, testMember, productOrders.get(2)));
-        reviewRepository.save(Review.create("test-review4", 4.0, testMember, productOrders.get(3)));
-        reviewRepository.save(Review.create("test-review5", 5.0, testMember, productOrders.get(4)));
-        reviewRepository.save(Review.create("test-review6", 2.5, testMember, productOrders.get(5)));
-        reviewRepository.save(Review.create("test-review7", 3.5, testMember, productOrders.get(6)));
-    }
-
-    private void saveUser() {
-        testMember = memberRepository.save(
-                Member.create(
-                        Provider.create("0001", ProviderName.TEST),
-                        "신우주",
-                        "test@email.com",
-                        "010-000-0000",
-                        "20~29"
-                )
-        );
-        memberRepository.save(
-                Member.create(
-                        Provider.create("0002", ProviderName.TEST),
-                        "서지훈",
-                        "test@email.com",
-                        "010-000-0000",
-                        "20~29"
-                )
-        );
-        memberRepository.save(
-                Member.create(
-                        Provider.create("0003", ProviderName.TEST),
-                        "김혜연",
-                        "test@email.com",
-                        "010-000-0000",
-                        "20~29"
-                )
-        );
-        memberRepository.save(
-                Member.create(
-                        Provider.create("0004", ProviderName.TEST),
-                        "오명주",
-                        "test@email.com",
-                        "010-000-0000",
-                        "20~29"
-                )
-        );
-        memberRepository.save(
-                Member.create(
-                        Provider.create("0005", ProviderName.TEST),
-                        "최솔지",
-                        "test@email.com",
-                        "010-000-0000",
-                        "20~29"
-                )
-        );
     }
 
     public Product saveProduct(String[] productRow) {
@@ -185,8 +85,8 @@ public class DataLoader {
         Category category1 = categoryRepository.save(new Category("비스킷/샌드"));
         Category category2 = categoryRepository.save(new Category("스낵/봉지과자"));
         Category category3 = categoryRepository.save(new Category("박스과자"));
-        Category category4 = categoryRepository.save(new Category("시리얼/바"));
         Category category5 = categoryRepository.save(new Category("캔디/사탕/젤리"));
+        Category category4 = categoryRepository.save(new Category("시리얼/바"));
         Category category6 = categoryRepository.save(new Category("초콜릿"));
         Category category7 = categoryRepository.save(new Category("껌/자일리톨"));
         Category category8 = categoryRepository.save(new Category("선물세트"));
